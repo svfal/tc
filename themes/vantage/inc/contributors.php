d714b9db1c1a9b4b4ff1fd4a7e5269d6aa2ecc35
svfal <s.falkenhain@aol.de>
2022-05-01 20:40:34 +0200

    Second theme

---
+++ themes/vantage/inc/contributors.php
@@ -0,0 +1,80 @@
+<?php
+return json_decode( '{
+	"b7122312e4008f8f2c76911080bca01a": {
+		"name": "Andrew Misplon",
+		"email": "b7122312e4008f8f2c76911080bca01a",
+		"loc": 7165,
+		"score": 129.585961622916,
+		"percent": 64.01104592600423
+	},
+	"5cff8d4385a469d1baab761889ad3161": {
+		"name": "Alex S",
+		"email": "5cff8d4385a469d1baab761889ad3161",
+		"loc": 1480,
+		"score": 67.62243367947801,
+		"percent": 33.403176190342634
+	},
+	"85ce7a450a7ee4895f3bd823505e2e12": {
+		"name": "adiraomj",
+		"email": "85ce7a450a7ee4895f3bd823505e2e12",
+		"loc": 1605,
+		"score": 3.1104480442135922,
+		"percent": 1.5364552619363123
+	},
+	"43c0e7220c23bd6636cf5469be6e9612": {
+		"name": "Braam Genis",
+		"email": "43c0e7220c23bd6636cf5469be6e9612",
+		"loc": 161,
+		"score": 0.9466480422460166,
+		"percent": 0.4676118504587766
+	},
+	"36639e6e074b731b093bde5a77305179": {
+		"name": "Braam Genis",
+		"email": "36639e6e074b731b093bde5a77305179",
+		"loc": 588,
+		"score": 0.3832617726326917,
+		"percent": 0.18931824576077078
+	},
+	"be6a5d2323cfe9ec8246b7a677dbc65b": {
+		"name": "Aaron Evans",
+		"email": "be6a5d2323cfe9ec8246b7a677dbc65b",
+		"loc": 23,
+		"score": 0.2617123909463051,
+		"percent": 0.12927699626149783
+	},
+	"9e7a3b2d24c2b15c53209ba8e7b4e724": {
+		"name": "Kevin Batdorf",
+		"email": "9e7a3b2d24c2b15c53209ba8e7b4e724",
+		"loc": 22,
+		"score": 0.23490303447154295,
+		"percent": 0.11603408841052004
+	},
+	"d1bea7e3f82e561c135bf5a3d4f9f896": {
+		"name": "SiteOrigin Support",
+		"email": "d1bea7e3f82e561c135bf5a3d4f9f896",
+		"loc": 13,
+		"score": 0.15296225570690702,
+		"percent": 0.07555813802957913
+	},
+	"d203744279876c5eda043108e2611648": {
+		"name": "Alex S",
+		"email": "d203744279876c5eda043108e2611648",
+		"loc": 19,
+		"score": 0.09540421290116749,
+		"percent": 0.0471264277169277
+	},
+	"a885c7bd5442dd2acd8c3e42001332c6": {
+		"name": "Alex S",
+		"email": "a885c7bd5442dd2acd8c3e42001332c6",
+		"loc": 20,
+		"score": 0.04093050340999098,
+		"percent": 0.020218272880324746
+	},
+	"07294cc1dd7658895e44c559dfffd76f": {
+		"name": "gpriday",
+		"email": "07294cc1dd7658895e44c559dfffd76f",
+		"loc": 20,
+		"score": 0.008459292865601918,
+		"percent": 0.004178602198417514
+	}
+}', true );
